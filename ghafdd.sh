7f156d0586dc639444183fa4a6791fe98725c708
Emrah Billur <emrah.billur@unikie.com>
2025-04-16 17:29:50 +0300

    Safer device detection

--- ghafdd.sh
+++ ghafdd.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/run/current-system/sw/bin/bash
 #Check if run by root user or sudo
 if [ "$EUID" -ne 0 ]
   then echo "Please run as root. Needed for flashing usb."
@@ -12,14 +12,14 @@ if [ -d "./result/sd-image" ] && [ -n "$(ls -A "./result/sd-image")" ]; then
    echo "The image is " $FILE
 else 
    echo "Image file not found!!!"
-   echo "Please run nix build to create the disk image and ensure you are in the build directory"   
+   echo "Please run nix build to create the disk image and ensure you are in the build directory" 
    exit 1
 fi
 echo "Detected the image file: " $FILE
 echo "Checking the required minimum size for USB drive"
 zstdcat "./"$FILE |pv -b >/dev/null
 
-echo "\n Please insert a usb drive to flash ghaf image and Press any key to continue..."
+echo "Please insert a usb drive to flash ghaf image and Press any key to continue..."
  
 # -s: Do not echo input coming from a terminal
 # -n 1: Read one character
@@ -27,8 +27,12 @@ read -s -n 1
  
 echo "You pressed a key! Continuing..."
 sleep 2
-DRIVE=$(dmesg |grep -o sd[a-z]: |tail -n1)
-DRIVE=${DRIVE::-1}
+# Old and unsafe method is now replaced
+#DRIVE=$(dmesg |grep -o sd[a-z]: |tail -n1)
+#DRIVE=${DRIVE::-1}
+# New detection method
+DRIVE=$(lsblk -d -n -oNAME,RO | grep '0$' | awk {'print $1'} |grep sd)
+echo $DEVICE
 DRIVE="/dev/"$DRIVE
 DEVICE1=$DRIVE"1"
 DEVICE2=$DRIVE"2"
@@ -54,8 +58,8 @@ done
 echo "Device "$DEVICE2 "is safe"
 
 echo "Writing the image on USB"
-zstdcat -v $FILE > $DRIVE 
-
+zstdcat -v $FILE |pv -b > $DRIVE 
+sync
 echo "Successfully written image to USB"
 exit 0
 
